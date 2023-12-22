1310c019006563e0416381f59c31d452f2c31f41
abdullah ibrahim Mohamed <144289227+20abdullahib@users.noreply.github.com>
2023-12-22 20:58:18 +0200

    Create custom_lib.h

---
+++ custom_lib.h
@@ -0,0 +1,12 @@
+// library_short.h
+#ifndef library_short
+#define library_short
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#endif 
